0e79c3c617241e0efd4db7980640de0debf14167
Anton_Yashchenko <151663096+AntoXa32@users.noreply.github.com>
2024-08-29 11:18:43 +0300

    Initial commit

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,11 @@
+import createFakeContact from '../utils/createFakeContact.js';
+import { getAllContacts } from './getAllContacts.js';
+import updateContacts from './updateContacts.js';
+const generateContacts = async (number) => {
+  const contactList = await getAllContacts();
+  const newContactList = Array(number).fill(0).map(createFakeContact);
+  contactList.push(...newContactList);
+  await updateContacts(contactList);
+};
+
+generateContacts(5);
