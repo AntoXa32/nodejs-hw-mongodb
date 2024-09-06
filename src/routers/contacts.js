e74f3aad3e467b716ec2e5aef4b545d846cb1aa2
AntoXa32 <151663096+AntoXa32@users.noreply.github.com>
2024-09-06 21:09:18 +0300

    run error

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,33 +1,14 @@
 import { Router } from 'express';
-import * as contactServices from './services/contacts.js';
+import {
+  getAllContactsController,
+  getContactByIdController,
+} from '../controllers/contacts.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 const contactsRouter = Router();
 
-contactsRouter.get('/', async (req, res) => {
-  const data = await contactServices.getAllContacts();
+contactsRouter.get('/', ctrlWrapper(getAllContactsController));
 
-  res.json({
-    status: 200,
-    message: 'Successfully found contacts!',
-    data,
-  });
-});
-
-contactsRouter.get('/:contactId', async (req, res) => {
-  const { contactId } = req.params;
-  const data = await contactServices.getContactById(contactId);
-
-  if (!data) {
-    return res.status(404).json({
-      message: `Contact with id=${contactId} not found`,
-    });
-  }
-
-  res.json({
-    status: 200,
-    message: `Contact with ${contactId} successfully find`,
-    data,
-  });
-});
+contactsRouter.get('/:contactId', ctrlWrapper(getContactByIdController));
 
 export default contactsRouter;
