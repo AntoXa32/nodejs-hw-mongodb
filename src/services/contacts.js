05f5a92b970ed87351a097b4d5daf6dba91dcf53
Anton_Yashchenko <151663096+AntoXa32@users.noreply.github.com>
2024-09-21 19:39:05 +0300

    add fixed

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -52,11 +52,12 @@ export const createContact = (payload) => ContactCollection.create(payload);
 
 export const updateContact = async (filter, data, options = {}) => {
   const rawResult = await ContactCollection.findOneAndUpdate(filter, data, {
+    new: true,
     includeResultMetadata: true,
     ...options,
   });
 
-  if (!rawResult || !rawResult.value) return null;
+  if (!rawResult) return null;
   return {
     data: rawResult.value,
     isNew: Boolean(rawResult?.lastErrorObject?.upserted),
